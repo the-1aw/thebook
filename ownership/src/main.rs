acac6f6d7f8d5fffe1033eadd407f7cd7a73574d
Bryan Guilliod <bryan.guilliod@globalfundwatch.com>
2024-11-22 14:06:44 +0100

    feat(ownership): basics of borrowing through references

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -14,6 +14,14 @@ fn is_this_go_or_rust(s: String) -> (usize, String) {
     (s.len(), s)
 }
 
+fn dont_take_ownership(str: &String) -> usize {
+    str.len()
+}
+
+fn mutable_borrowing(str: &mut String) {
+    str.push_str("+sneaky+");
+}
+
 fn main() {
     let s1 = String::from("hello");
     take_ownership(s1);
@@ -24,4 +32,9 @@ fn main() {
     let new_s2 = take_then_give_back(new_s2);
     let (len, str) = is_this_go_or_rust(new_s2);
     println!("str contains \"{str}\" and has len: {len}");
+    let len = dont_take_ownership(&str);
+    println!("can still use \"{str}\" and it still has len: {len}");
+    let mut s3 = String::from("borrow me");
+    mutable_borrowing(&mut s3);
+    println!("value from s3 is now: {s3}");
 }
