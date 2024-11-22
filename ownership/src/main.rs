aacfab07ed9343521433b9901d00191c1e36b776
the-1aw <bguilliod@gmail.com>
2024-11-22 19:46:00 +0100

    feat(ownership): basics of slices

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -22,6 +22,39 @@ fn mutable_borrowing(str: &mut String) {
     str.push_str("+sneaky+");
 }
 
+// For all the first word variant we must asume str is ascii based
+fn first_word(str: &String) -> &str {
+    let bytes = str.as_bytes();
+    let mut len = str.len();
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            len = i;
+            break;
+        }
+    }
+    return &str[..len];
+}
+
+fn first_word_no_mut(str: &String) -> &str {
+    let bytes = str.as_bytes();
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &str[..i];
+        }
+    }
+    &str[..]
+}
+
+fn first_word_no_mut_good_signature(str: &str) -> &str {
+    let bytes = str.as_bytes();
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &str[..i];
+        }
+    }
+    &str[..]
+}
+
 fn main() {
     let s1 = String::from("hello");
     take_ownership(s1);
@@ -37,4 +70,13 @@ fn main() {
     let mut s3 = String::from("borrow me");
     mutable_borrowing(&mut s3);
     println!("value from s3 is now: {s3}");
+    println!("the first word is {}", first_word(&String::from("one two")));
+    println!(
+        "the first word is also {}",
+        first_word_no_mut(&String::from("one two"))
+    );
+    println!(
+        "the first word is still {}",
+        first_word_no_mut_good_signature("one two")
+    );
 }
