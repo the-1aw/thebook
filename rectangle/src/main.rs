21658fa3fe6dc29c41e5ffb6a25fe0a3e400da02
the-1aw <bguilliod@gmail.com>
2024-11-23 19:53:57 +0100

    feat(struct): basics of associated functions

--- rectangle/src/main.rs
+++ rectangle/src/main.rs
@@ -14,6 +14,27 @@ fn uarea(urect: &UglyRect) -> usize {
     urect.0 * urect.1
 }
 
+impl Rectangle {
+    fn area(&self) -> usize {
+        self.width * self.height
+    }
+
+    fn width(&self) -> bool {
+        self.width > 0
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+
+    fn square(size: usize) -> Rectangle {
+        Rectangle {
+            width: size,
+            height: size,
+        }
+    }
+}
+
 fn main() {
     println!(
         "The area of the ugly rectangle is {} square pixels.",
@@ -46,4 +67,15 @@ fn main() {
         height: dbg!(12 * scale),
     };
     dbg!(&rect);
+    println!("The area from method is {} square pixels.", rect.area());
+    if rect.width() {
+        println!("Rect as non zero width: {}", rect.width);
+    }
+    let rect2 = Rectangle {
+        width: 10,
+        height: 10,
+    };
+    println!("Can rect hold rect2 ? {}", rect.can_hold(&rect2));
+    println!("Can rect2 hold rect ? {}", rect2.can_hold(&rect));
+    dbg!(&Rectangle::square(4));
 }
