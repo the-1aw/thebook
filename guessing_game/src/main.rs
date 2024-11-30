35b79b3af6237c601ade5dfbd37645cdc0082078
the-1aw <bguilliod@gmail.com>
2024-11-30 20:18:11 +0100

    feat(erros): update guessing_game to showcase Custom Type usage for input validation

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,6 +1,23 @@
 use rand::Rng;
 use std::{cmp::Ordering, io};
 
+struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Guess value must be between 1 and 100, got: {value}.");
+        }
+        Guess { value }
+    }
+
+    pub fn value(&self) -> i32 {
+        self.value
+    }
+}
+
 fn main() {
     println!("Guess the number!");
     let secret_num = rand::thread_rng().gen_range(1..=100);
@@ -10,12 +27,13 @@ fn main() {
         io::stdin()
             .read_line(&mut user_guess)
             .expect("Failed to read line");
-        let user_guess: u32 = match user_guess.trim().parse() {
+        let user_guess: i32 = match user_guess.trim().parse() {
             Ok(num) => num,
             Err(_) => continue,
         };
+        let guess = Guess::new(user_guess);
         println!("You guessed: {user_guess}");
-        match user_guess.cmp(&secret_num) {
+        match guess.value().cmp(&secret_num) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
             Ordering::Equal => {
