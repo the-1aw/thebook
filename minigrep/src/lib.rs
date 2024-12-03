77e92491be04d56e0bb024546000e255e604b2cb
the-1aw <bguilliod@gmail.com>
2024-12-03 22:45:20 +0100

    refactor(minigrep): move logic bits from main to lib

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -1,3 +1,38 @@
+pub struct Config {
+    pattern: String,
+    path_list: Vec<String>,
+}
+
+impl Config {
+    pub fn new(args: &[String]) -> Result<Config, &'static str> {
+        let pattern = match args.get(1) {
+            Some(pattern) => pattern,
+            _ => return Err("this is where the usage should go"),
+        };
+        let path_list = &args[2..];
+        Ok(Config {
+            pattern: pattern.to_string(),
+            path_list: path_list.to_vec(),
+        })
+    }
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn std::error::Error>> {
+    if config.path_list.len() > 0 {
+        println!(
+            "Searching for \"{}\" in files {:?}",
+            config.pattern, config.path_list
+        );
+        for path in config.path_list {
+            let contents = std::fs::read_to_string(&path)?;
+            println!("File {path} contains test:\n{contents}");
+        }
+    } else {
+        println!("In the end this will search in stdin");
+    }
+    Ok(())
+}
+
 #[cfg(test)]
 mod tests {
     // use super::*;
--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -7,52 +7,20 @@
 // it would also be nice to have a usage
 // (Maybe if the mood is there we'll do --help option at the end)
 
-struct Config {
-    pattern: String,
-    path_list: Vec<String>,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        let pattern = match args.get(1) {
-            Some(pattern) => pattern,
-            _ => return Err("this is where the usage should go"),
-        };
-        let path_list = &args[2..];
-        Ok(Config {
-            pattern: pattern.to_string(),
-            path_list: path_list.to_vec(),
-        })
-    }
-}
-
-fn run(config: Config) -> Result<(), Box<dyn std::error::Error>> {
-    if config.path_list.len() > 0 {
-        println!(
-            "Searching for \"{}\" in files {:?}",
-            config.pattern, config.path_list
-        );
-        for path in config.path_list {
-            let contents = std::fs::read_to_string(&path)?;
-            println!("File {path} contains test:\n{contents}");
-        }
-    } else {
-        println!("In the end this will search in stdin");
-    }
-    Ok(())
-}
+use minigrep::Config;
+use std::{env, process};
 
 fn main() {
-    let args: Vec<String> = std::env::args().collect();
+    let args: Vec<String> = env::args().collect();
     let config = match Config::new(&args) {
         Ok(config) => config,
         Err(err) => {
             eprintln!("{}", err);
-            std::process::exit(1);
+            process::exit(1);
         }
     };
-    if let Err(err) = run(config) {
+    if let Err(err) = minigrep::run(config) {
         eprintln!("Oops! An error occured: {err}");
-        std::process::exit(1);
+        process::exit(1);
     };
 }
