22417f2d949c67bbd2eef01b031272e18b880164
Bryan Guilliod <bryan.guilliod@globalfundwatch.com>
2024-12-03 21:21:35 +0100

    refactor(minigrep): siplit main logic in small bit and add error handling

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -7,19 +7,52 @@
 // it would also be nice to have a usage
 // (Maybe if the mood is there we'll do --help option at the end)
 
-fn main() {
-    let args: Vec<String> = std::env::args().collect();
-    let pattern = args.get(1).expect("This is where the usage will go");
-    let path_list = &args[2..];
+struct Config {
+    pattern: String,
+    path_list: Vec<String>,
+}
 
-    if path_list.len() > 0 {
-        println!("Searching for \"{pattern}\" in files {path_list:?}");
-        for path in path_list {
-            let contents =
-                std::fs::read_to_string(path).expect(&format!("Unable to read file {path}"));
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        let pattern = match args.get(1) {
+            Some(pattern) => pattern,
+            _ => return Err("this is where the usage should go"),
+        };
+        let path_list = &args[2..];
+        Ok(Config {
+            pattern: pattern.to_string(),
+            path_list: path_list.to_vec(),
+        })
+    }
+}
+
+fn run(config: Config) -> Result<(), Box<dyn std::error::Error>> {
+    if config.path_list.len() > 0 {
+        println!(
+            "Searching for \"{}\" in files {:?}",
+            config.pattern, config.path_list
+        );
+        for path in config.path_list {
+            let contents = std::fs::read_to_string(&path)?;
             println!("File {path} contains test:\n{contents}");
         }
     } else {
         println!("In the end this will search in stdin");
     }
+    Ok(())
+}
+
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
+    let config = match Config::new(&args) {
+        Ok(config) => config,
+        Err(err) => {
+            eprintln!("{}", err);
+            std::process::exit(1);
+        }
+    };
+    if let Err(err) = run(config) {
+        eprintln!("Oops! An error occured: {err}");
+        std::process::exit(1);
+    };
 }
