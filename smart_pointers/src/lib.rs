627430b12f9fcaba0402beefe9cdb83e2c267f59
the-1aw <bguilliod@gmail.com>
2024-12-22 16:12:46 +0530

    feat(smart-pointers): basics of deref and deref coercion

--- smart_pointers/src/lib.rs
+++ smart_pointers/src/lib.rs
@@ -3,6 +3,29 @@ pub enum ConsList {
     Cons(u32, Box<ConsList>),
     Nil,
 }
+pub struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    pub fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
+use std::ops::{Deref, DerefMut};
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl<T> DerefMut for MyBox<T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
 
 #[cfg(test)]
 mod tests {
@@ -22,5 +45,27 @@ mod tests {
             curr = next;
         }
         assert_eq!(*curr, Nil);
+    }
+
+    #[test]
+    fn my_box() {
+        let val = 9;
+        let my_box = MyBox::new(val);
+        assert_eq!(val, 9);
+        assert_eq!(*my_box, 9);
+
+        fn hello(str: &str) {
+            println!("{str}")
+        }
+
+        fn hello_mut(str: &mut str) {
+            println!("{str}")
+        }
+
+        let my_str_box = MyBox::new(String::from("Rust"));
+        hello(&my_str_box);
+        let mut my_mut_str_box = MyBox::new(String::from("Rust"));
+        hello(&my_mut_str_box);
+        hello_mut(&mut my_mut_str_box);
     }
 }
