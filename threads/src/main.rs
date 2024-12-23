9d7587d4414a1ff033477438d89e0b48d9a4646a
the-1aw <bguilliod@gmail.com>
2024-12-23 23:26:11 +0100

    feat(concurrency): introduction to sync::mpsc(multiple producer single consumer) channels

--- threads/src/main.rs
+++ threads/src/main.rs
@@ -1,4 +1,4 @@
-use std::{thread, time::Duration};
+use std::{sync::mpsc, thread, time::Duration};
 
 fn spawn_thread_explicit_move() -> thread::JoinHandle<Vec<usize>> {
     let mut v = vec![];
@@ -32,6 +32,15 @@ fn spawn_thread_implicit_move() -> thread::JoinHandle<Vec<usize>> {
     })
 }
 
+fn spawn_thread_send_stuff(tx: mpsc::Sender<String>) {
+    thread::spawn(move || {
+        let stuff = vec!["send".to_string(), "from".to_string(), "string".to_string()];
+        for s in stuff {
+            tx.send(s).unwrap();
+        }
+    });
+}
+
 fn main() {
     let i_handle = spawn_thread_implicit_move();
     let e_handle = spawn_thread_explicit_move();
@@ -43,4 +52,16 @@ fn main() {
     let i_v = i_handle.join().unwrap();
     assert_eq!(vec![1, 2, 3, 4, 5], e_v);
     assert_eq!(vec![1, 2, 3, 4, 5], i_v);
+
+    let (tx, rx) = mpsc::channel();
+    // clone to allow multiple producers
+    spawn_thread_send_stuff(tx.clone());
+    // move so channel is closed at the end of spawn_thread_send_stuff
+    spawn_thread_send_stuff(tx);
+    let mut rx_buffer = Vec::new();
+    for receive in rx {
+        println!("got: {receive}");
+        rx_buffer.push(receive);
+    }
+    assert_eq!(rx_buffer.len(), 6);
 }
