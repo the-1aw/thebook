252543d2ec1e99eea02a79580025ed18583f6fec
the-1aw <bguilliod@gmail.com>
2024-12-25 21:26:51 +0100

    feat(OOP): add lib test to ensure enum and state pattern post implementation are interchangeable

--- blog/src/lib.rs
+++ blog/src/lib.rs
@@ -1,2 +1,25 @@
 pub mod enums;
 pub mod state_pattern;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn ensure_compatibility() {
+        let mut e_post = enums::Post::new();
+        let mut sp_post = state_pattern::Post::new();
+
+        e_post.add_text("I hurt myself today");
+        sp_post.add_text("I hurt myself today");
+        assert_eq!(e_post.content(), sp_post.content());
+
+        e_post.request_review();
+        sp_post.request_review();
+        assert_eq!(e_post.content(), sp_post.content());
+
+        e_post.approve();
+        sp_post.approve();
+        assert_eq!(e_post.content(), sp_post.content());
+    }
+}
