b2969249308eb90108aa7ab821361d75b097bd22
Bryan Guilliod <bryan.guilliod@globalfundwatch.com>
2024-12-26 18:35:45 +0100

    feat(OOP): add multiple approval requirement to state as types implementation

--- blog/src/main.rs
+++ blog/src/main.rs
@@ -1,13 +1,20 @@
-use blog::rs_state_pattern;
+use blog::rs_state_pattern::{Approval, Post};
 
 fn main() {
-    let mut post = rs_state_pattern::Post::new();
+    let mut post = Post::new();
 
     post.add_text("I hurt myself today");
 
     let post = post.request_review();
 
-    let post = post.approve();
+    let mut post = post.approve();
+    while let Approval::Pending(pending) = post {
+        post = pending.approve();
+    }
 
-    assert_eq!("I hurt myself today", post.content());
+    assert!(matches!(post, Approval::Confirmed(_)));
+    match post {
+        Approval::Confirmed(post) => assert_eq!("I hurt myself today", post.content()),
+        _ => (),
+    };
 }
--- blog/src/rs_state_pattern.rs
+++ blog/src/rs_state_pattern.rs
@@ -8,6 +8,7 @@ pub struct DraftPost {
 
 pub struct PendingReviewPost {
     content: String,
+    nb_approval: usize,
 }
 
 impl Post {
@@ -30,14 +31,28 @@ impl DraftPost {
     pub fn request_review(self) -> PendingReviewPost {
         PendingReviewPost {
             content: self.content,
+            nb_approval: 0,
         }
     }
 }
 
+pub enum Approval {
+    Confirmed(Post),
+    Pending(PendingReviewPost),
+}
+
 impl PendingReviewPost {
-    pub fn approve(self) -> Post {
-        Post {
-            content: self.content,
+    pub fn approve(mut self) -> Approval {
+        self.nb_approval += 1;
+        if self.nb_approval < 2 {
+            Approval::Pending(PendingReviewPost {
+                content: self.content,
+                nb_approval: self.nb_approval,
+            })
+        } else {
+            Approval::Confirmed(Post {
+                content: self.content,
+            })
         }
     }
 
