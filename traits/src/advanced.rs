88f6eede9fddb675ca76a0423140b6771f3eb24c
the-1aw <bguilliod@gmail.com>
2025-01-01 18:25:35 +0100

    feat(traits): play with fully qualified syntax

--- traits/src/advanced.rs
+++ traits/src/advanced.rs
@@ -7,13 +7,13 @@ pub trait Demonize {
 }
 
 #[derive(Debug, PartialEq)]
-struct Point {
+pub struct Point {
     x: u32,
     y: u32,
 }
 
 #[derive(Debug, PartialEq)]
-struct EvilPoint {
+pub struct EvilPoint {
     x: u32,
     y: u32,
     z: u32,
@@ -52,6 +52,31 @@ impl Add<EvilPoint> for Point {
     }
 }
 
+pub trait Ambiguous {
+    fn ambiguous_method(&self) -> usize;
+    fn ambiguous_static() -> usize;
+}
+
+impl Ambiguous for Point {
+    fn ambiguous_method(&self) -> usize {
+        6
+    }
+
+    fn ambiguous_static() -> usize {
+        9
+    }
+}
+
+impl Point {
+    pub fn ambiguous_method(&self) -> usize {
+        4
+    }
+
+    pub fn ambiguous_static() -> usize {
+        20
+    }
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -76,5 +101,18 @@ mod test {
             Point { x: 2, y: 10 } + EvilPoint { x: 2, y: 10, z: 69 },
             Point { x: 4, y: 20 }
         );
+    }
+
+    #[test]
+    fn call_ambiguous_methods() {
+        let p = Point { x: 0, y: 0 };
+        assert_eq!(p.ambiguous_method(), 4);
+        assert_eq!(Ambiguous::ambiguous_method(&p), 6);
+    }
+
+    #[test]
+    fn call_ambiguous_statics() {
+        assert_eq!(Point::ambiguous_static(), 20);
+        assert_eq!(<Point as Ambiguous>::ambiguous_static(), 9);
     }
 }
