f005ea62af611ab3e01ba0622c53a7e13da5f4f2
the-1aw <bguilliod@gmail.com>
2025-01-05 11:41:14 +0100

    feat(macros): brief introduction to function-like and attribute-like macros

--- macros/macros_derive/src/lib.rs
+++ macros/macros_derive/src/lib.rs
@@ -12,6 +12,11 @@ fn impl_demo_macro(ast: &syn::DeriveInput) -> TokenStream {
     })
 }
 
+/// This is a demo macro to showcase custom derive macro
+///
+/// This implements a dummy MacroDemo trait for the specific struct or enum provided
+/// in this case it will print "Hello, macro! My name is <TypeName>"
+/// where <TypeName> is the name of the provided struct or enum
 #[proc_macro_derive(MacroDemo)]
 pub fn derive_demo_macro(input: TokenStream) -> TokenStream {
     // For more info on why this is used instead of syn::parse().unwrapt()
@@ -19,4 +24,25 @@ pub fn derive_demo_macro(input: TokenStream) -> TokenStream {
     let ast = syn::parse_macro_input!(input);
 
     impl_demo_macro(&ast)
+}
+
+/// This is a demo macro to showcase a Attribute-like macro
+///
+/// This is a dummy implementation but irl it could for example
+/// 1) Parse attr and its args to get route info
+/// 2) Parse item and make sure it's a function
+/// 3) Generate implementation for a http handle that call the item fn when triggered
+#[proc_macro_attribute]
+pub fn route(_attr: TokenStream, item: TokenStream) -> TokenStream {
+    item
+}
+
+/// This is a demo macro showcase a Function-like macro
+///
+/// This is a dummy implementation but irl it could for example
+/// 1) Parse a raw SQL query in the _input
+/// 2) Generate implementation for the sql query from our lib
+#[proc_macro]
+pub fn sql(input: TokenStream) -> TokenStream {
+    input
 }
